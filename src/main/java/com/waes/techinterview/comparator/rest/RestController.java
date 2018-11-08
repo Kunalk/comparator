74d2bafda5df6dbb46002850103be6306cdc0fb0
kunalk <kunalkhanvilkar.pune@gmail.com>
2018-11-08 09:16:43 +0530

    controller changes

--- src/main/java/com/waes/techinterview/comparator/rest/RestController.java
+++ src/main/java/com/waes/techinterview/comparator/rest/RestController.java
@@ -6,16 +6,24 @@ import com.waes.techinterview.comparator.service.storage.StorageService;
 import com.waes.techinterview.comparator.service.validator.ValidationService;
 import com.waes.techinterview.comparator.vo.ComparatorInputSideEnum;
 import com.waes.techinterview.comparator.vo.ComparatorInputVO;
+import com.waes.techinterview.comparator.vo.ComparatorResultVO;
 import com.waes.techinterview.comparator.vo.RequestObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.trace.http.HttpTrace;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.xml.ws.Response;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Application Rest endpoints.
@@ -59,7 +67,7 @@ public class RestController {
         storageService.store(comparatorInputVO);
 
 
-        return new ResponseEntity<String>(HttpStatus.CREATED);
+        return new ResponseEntity<String>("Data added successfully for id - "+id, HttpStatus.CREATED);
     }
 
     /**
@@ -76,7 +84,7 @@ public class RestController {
         ComparatorInputVO comparatorInputVO = new ComparatorInputVO(id, data.getData(), ComparatorInputSideEnum.RIGHT);
         storageService.store(comparatorInputVO);
 
-        return null;
+        return new ResponseEntity<String>("Data added successfully for id - "+id,HttpStatus.CREATED);
     }
 
     /**
@@ -85,7 +93,15 @@ public class RestController {
      * @return HTTP Response 200: OK <p>
      */
     @RequestMapping(method = RequestMethod.GET, produces = "application/json")
-    private ResponseEntity<String> diff(@PathVariable Long id) {
-        return null;
+    private ResponseEntity<ComparatorResultVO> diff(@PathVariable Long id) {
+
+        ComparatorResultVO comparatorResultVO = comparatorService.compare(id);
+
+        if(comparatorResultVO.getComparatorResultEnum()!=null){
+            return new ResponseEntity<ComparatorResultVO>(comparatorResultVO,HttpStatus.OK);
+        }else{
+            return new ResponseEntity<ComparatorResultVO>(comparatorResultVO,HttpStatus.NOT_FOUND);
+        }
+
     }
 }
